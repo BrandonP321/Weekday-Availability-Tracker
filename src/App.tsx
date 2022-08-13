5835ec35e3485598c9ce11f2916a94682c600274
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-08-12 22:44:56 -0700

    Cleaned up boilerplate CRA code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import "destyle.css";
+import './App.module.scss';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AvailabilityTracker from './pages/AvailabilityTracker/AvailabilityTracker';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path={"/"} element={<AvailabilityTracker />} />
+          {/* All 404's should redirect back to home page */}
+          <Route path={"*"} element={<Navigate replace to={"/"} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
